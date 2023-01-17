0cb8d100de42fb0e43c616f98043e8180252eec4
Qu-Abdullah <117937150+Qu-Abdullah@users.noreply.github.com>
2023-01-17 12:03:35 -0500

    Create Background
    New Background to match my new games theme

--- MyWorld.java
+++ MyWorld.java
@@ -16,9 +16,10 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
-        WaterGun wg = new WaterGun();
-        addObject(wg,0,0);
+        super(550, 350, 1);
+
+        Bullet bullet = new Bullet();
+        addObject(bullet, getWidth()/2, getHeight()/2);
 
         
     }
