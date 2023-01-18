28f020bf063759b0c16ec851b56c8169e9ab7147
Qu-Abdullah <117937150+Qu-Abdullah@users.noreply.github.com>
2023-01-18 00:05:46 -0500

    Update MyWorld.java
    Added increasing difficulty
    
    Added a Game Over method

--- MyWorld.java
+++ MyWorld.java
@@ -3,11 +3,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class MyWorld here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @Qureshi
+ * @17 January 2023
  */
 public class MyWorld extends World
 {
+    int counter = 0;
+    double amplifier = 1.00;
+    int score;
 
     /**
      * Constructor for objects of class MyWorld.
@@ -16,11 +19,47 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(550, 350, 1);
-
-        Bullet bullet = new Bullet();
-        addObject(bullet, getWidth()/2, getHeight()/2);
+        super(600, 400, 1, false);
 
         
+        Character character = new Character();
+        addObject(character, getWidth()/2, getHeight()/2);
+
+                
+    }
+
+    
+    public void act(){
+        
+        counter++;
+        if(counter == 100*amplifier){
+            Obstacles obstacle = new Obstacles();
+            Ghost ghost = new Ghost();
+            
+            addObject(obstacle, getWidth(), getHeight()/2 + obstacle.getImage().getHeight() - Greenfoot.getRandomNumber(400));
+            addObject(ghost, getWidth(), Greenfoot.getRandomNumber(400));
+            
+            counter = 0;
+            score++;
+            
+            if(score == 10){
+                
+                if(amplifier > 0.75){
+                    amplifier -=0.25;
+                }
+                
+                score = 0;
+                
+            }
+            
+        }
+    }
+    
+    public void gameOver(){
+        Label gameOver = new Label("Game Over",100);
+        
+        addObject(gameOver, getWidth()/2, getHeight()/2);
+        
+        Greenfoot.stop();
     }
 }
